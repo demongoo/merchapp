f0f684d4a3be30b94e8d4570ac0dbc5630885ec3
Dmitry Revenko <dmitry.revenko@gmail.com>
2017-04-22 14:56:09 +0300

    Finally damn pane resize

--- src/main/scala/FXApp.scala
+++ src/main/scala/FXApp.scala
@@ -7,14 +7,15 @@ package me.demongoo.merchapp
 import scalafx.Includes._
 import scalafx.application.JFXApp
 import scalafx.application.JFXApp.PrimaryStage
-import scalafx.geometry.{Insets, NodeOrientation}
+import scalafx.geometry.{Insets, NodeOrientation, Pos}
 import scalafx.scene.Scene
 import scalafx.scene.effect.DropShadow
-import scalafx.scene.layout.HBox
-import scalafx.scene.paint.Color._
+import scalafx.scene.layout._
 import scalafx.scene.paint._
-import scalafx.scene.text.Text
-import scalafx.scene.control.TextArea
+import scalafx.scene.control._
+import scalafx.collections.ObservableBuffer
+import scalafx.event._
+import scalafx.scene.input._
 
 object FXApp extends JFXApp {
   // app init
@@ -84,24 +85,78 @@ object FXApp extends JFXApp {
 
   val ta = new TextArea {
     id = "ta"
+    alignmentInParent = Pos.Center
+    vgrow = Priority.Always
+    hgrow = Priority.Always
+  }
 
+  val bp = new BorderPane {
+    style = "-fx-base: yellow"
+
+    center = new VBox {
+      style = "-fx-background: blue"
+      spacing = 10
+      fillWidth = true
+      children = Seq(
+        new MenuBar {
+          menus = Seq(
+            new Menu("One") {
+              items = List("What", "The", "Fuck") map (new MenuItem(_))
+            },
+            new Menu("Two") {
+              items = List("Was", "ist", "das") map (new MenuItem(_))
+            },
+            new Menu("Exit") {
+              accelerator = KeyCombination.keyCombination("Ctrl+C")
+              onShowing = handle {
+                System.exit(0)
+              }
+            }
+          )
+        },
+        ta,
+        new Button("Fuck You") {
+          onMouseClicked = handle {
+            sqpla()
+          }
+          onMouseEntered = handle {
+            effect = new DropShadow()
+          }
+          onMouseExited = handle {
+            effect = null
+          }
+        },
+        new HBox {
+          spacing = 5
+          alignment = Pos.CenterLeft
+          children = Seq(
+            new Label("Place"),
+            new ChoiceBox[String] {
+              items = ObservableBuffer("Earth", "Sky", "Paradise")
+            }
+          )
+        },
+        new TextField {
+          promptText = "Eto i pravda nechto"
+          onMouseClicked = (evt: Event) => text = evt.toString
+        }
+      )
+    }
   }
 
   stage = new PrimaryStage {
-    title = "ScalaFX Hello World"
+    title = "Merchant App 1.0"
     maximized = true
+    
     scene = new Scene {
-      fill = Color.rgb(38, 38, 38)
-      content = new HBox {
-        padding = Insets(50, 80, 50, 80)
-        children = Seq(
-          ta
-        )
-      }
+      fill = Color.rgb(240, 240, 240)
+      maximized = true
+      stylesheets.add(getClass.getResource("/main.css").toExternalForm)
 
-      onMouseClicked = handle {
-        sqpla()
-      }
+      content = bp
     }
   }
+
+  bp.prefWidth <== stage.width
+  bp.prefHeight <== stage.height
 }
